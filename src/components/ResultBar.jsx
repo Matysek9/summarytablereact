aad561c9ef06e2c75181f30f00fc14d718475fa1
Mateusz W <mateuszwyc@gmail.com>
2023-04-19 11:54:31 +0200

    svg

--- src/components/ResultBar.jsx
+++ src/components/ResultBar.jsx
@@ -1,18 +1,18 @@
-import "./ResultBar.css"
+import "./ResultBar.css";
+import {ComponentIcon as icon} from "../assets/icon-visual.svg";
 
 function ResultBar(props) {
-  
-
   return (
     <div className={`bar ${props.color}`}>
       <div className="category flex gap-1">
-        <img src={props.img} alt="svg icon"  className="svg"/>
-     
+        <img src={props.img} alt="svg icon"/>
         <span>{props.category}</span>
       </div>
       <div className="score flex gap-05">
         <p>{props.score}</p>
-        <p>/100</p></div>
+        <p>/100</p>
+      </div>
+      {icon}
     </div>
   );
 }
